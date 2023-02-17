782f7b04012a36dc1f93ee1673eb892c1bdd99d7
s-stalwart-s <stalwart.programmer@gmail.com>
2023-02-17 21:40:25 +0330

    add tests

--- index.js
+++ index.js
@@ -1,174 +1,168 @@
 const globalConfigs = {
-	callerName: "unknownCaller",
-	canPrintError: true,
+  callerName: "unknownCaller",
+  canPrintError: true,
 };
 
 const CALLBACK_TYPES = {
-	ASYNC: "async",
-	SYNC: "sync",
+  ASYNC: "async",
+  SYNC: "sync",
 };
 
 class Trier {
-	static changeGlobalConfigs(newConfigs = globalConfigs) {
-		Object.entries(newConfigs).forEach(([configKey, configValue]) => {
-			globalConfigs[configKey] = configValue;
-		});
-	}
+  static changeGlobalConfigs(newConfigs = globalConfigs) {
+    Object.entries(newConfigs).forEach(([configKey, configValue]) => {
+      globalConfigs[configKey] = configValue;
+    });
+  }
 
-	#getDefaultCallbacks() {
-		return {
-			try: [],
-			catch: () => {},
-			executeIfNoError: () => {},
-			finally: () => {},
-		};
-	}
-	#callbacks = this.#getDefaultCallbacks();
+  #getDefaultCallbacks() {
+    return {
+      try: [],
+      catch: () => {},
+      executeIfNoError: () => {},
+      finally: () => {},
+    };
+  }
+  #callbacks = this.#getDefaultCallbacks();
 
-	#options = { ...globalConfigs, shouldThrowError: false };
-	#hasError = false;
-	#tryResult = undefined;
-	#catchResult = undefined;
+  #options = { ...globalConfigs, shouldThrowError: false };
+  #hasError = false;
+  #tryResult = undefined;
+  #catchResult = undefined;
 
-	constructor(callerName, options = globalConfigs) {
-		this.setOptions({ ...options, callerName });
-	}
+  constructor(callerName, options = globalConfigs) {
+    this.setOptions({ ...options, callerName });
+  }
 
-	getOptions() {
-		return this.#options;
-	}
-	setOptions(options = this.getOptions()) {
-		this.#options = { ...this.getOptions(), ...options };
-		return this;
-	}
+  getOptions() {
+    return this.#options;
+  }
+  setOptions(options = this.getOptions()) {
+    this.#options = { ...this.getOptions(), ...options };
+    return this;
+  }
 
-	try(callback, ...params) {
-		this.#callbacks.try.push({
-			type: CALLBACK_TYPES.SYNC,
-			callback: () => {
-				try {
-					this.#tryResult = callback(...params);
-				} catch (error) {
-					this.#handleCatchBlock(error);
-				}
-			},
-		});
+  try(callback, ...params) {
+    this.#callbacks.try.push({
+      type: CALLBACK_TYPES.SYNC,
+      callback: () => {
+        try {
+          this.#tryResult = callback(...params);
+        } catch (error) {
+          this.#handleCatchBlock(error);
+        }
+      },
+    });
 
-		return this;
-	}
-	tryAsync(callback, ...params) {
-		this.#callbacks.try.push({
-			type: CALLBACK_TYPES.ASYNC,
-			callback: async () => {
-				try {
-					this.#tryResult = await callback(...params);
-				} catch (error) {
-					this.#handleCatchBlock(error);
-				}
-			},
-		});
+    return this;
+  }
+  tryAsync(callback, ...params) {
+    this.#callbacks.try.push({
+      type: CALLBACK_TYPES.ASYNC,
+      callback: async () => {
+        try {
+          this.#tryResult = await callback(...params);
+        } catch (error) {
+          this.#handleCatchBlock(error);
+        }
+      },
+    });
 
-		return this;
-	}
+    return this;
+  }
 
-	#handleCatchBlock(error) {
-		this.#hasError = true;
-		this.#catchResult = error;
-		const { canPrintError } = this.getOptions();
+  #handleCatchBlock(error) {
+    this.#hasError = true;
+    this.#catchResult = error;
+    const { canPrintError } = this.getOptions();
 
-		if (canPrintError) this.#printError();
-	}
-	#printError() {
-		const { callerName } = this.getOptions();
+    if (canPrintError) this.#printError();
+  }
+  #printError() {
+    const { callerName } = this.getOptions();
 
-		printError(callerName, this.#catchResult);
+    printError(callerName, this.#catchResult);
 
-		return this;
-	}
+    return this;
+  }
 
-	throw() {
-		this.#options.shouldThrowError = true;
-		return this;
-	}
+  throw() {
+    this.#options.shouldThrowError = true;
+    return this;
+  }
 
-	executeIfNoError(callback, ...params) {
-		this.#callbacks.executeIfNoError = () => {
-			if (this.#hasError === false) {
-				callback(this.#tryResult, ...params);
-			}
-		};
+  executeIfNoError(callback, ...params) {
+    this.#callbacks.executeIfNoError = () => {
+      if (this.#hasError === false) {
+        callback(this.#tryResult, ...params);
+      }
+    };
 
-		return this;
-	}
+    return this;
+  }
 
-	catch(callback, ...params) {
-		this.#callbacks.catch = () => {
-			if (this.#hasError) {
-				this.#catchResult = callback(this.#catchResult, ...params);
-			}
-		};
+  catch(callback, ...params) {
+    this.#callbacks.catch = () => {
+      if (this.#hasError) {
+        this.#catchResult = callback(this.#catchResult, ...params);
+      }
+    };
 
-		return this;
-	}
+    return this;
+  }
 
-	finally(callback, ...params) {
-		this.#callbacks.finally = () => {
-			callback(this.#tryResult, ...params);
-		};
+  finally(callback, ...params) {
+    this.#callbacks.finally = () => {
+      callback(this.#tryResult, ...params);
+    };
 
-		return this;
-	}
+    return this;
+  }
 
-	run() {
-		const syncCallbacks = this.#callbacks.try.filter(
-			(i) => i.type === CALLBACK_TYPES.SYNC,
-		);
-		for (const i of syncCallbacks) {
-			if (this.#isErrorOccurred()) break;
-			i.callback();
-		}
+  run() {
+    const syncCallbacks = this.#callbacks.try.filter(
+      (i) => i.type === CALLBACK_TYPES.SYNC
+    );
+    for (const i of syncCallbacks) {
+      if (this.#isErrorOccurred()) break;
+      i.callback();
+    }
 
-		return this.#handleRestTasks();
-	}
+    return this.#handleRestTasks();
+  }
 
-	async runAsync() {
-		for (const i of this.#callbacks.try) {
-			if (this.#isErrorOccurred()) break;
-			await i.callback();
-		}
+  async runAsync() {
+    for (const i of this.#callbacks.try) {
+      if (this.#isErrorOccurred()) break;
+      await i.callback();
+    }
 
-		return this.#handleRestTasks();
-	}
+    return this.#handleRestTasks();
+  }
 
-	#isErrorOccurred() {
-		return this.#hasError;
-	}
+  #isErrorOccurred() {
+    return this.#hasError;
+  }
 
-	#handleRestTasks() {
-		if (this.#isErrorOccurred()) {
-			this.#callbacks.catch();
+  #handleRestTasks() {
+    if (this.#isErrorOccurred()) {
+      this.#callbacks.catch();
 
-			if (this.#options.shouldThrowError) throw this.#catchResult;
-		}
+      if (this.#options.shouldThrowError) throw this.#catchResult;
+    }
 
-		if (!this.#isErrorOccurred()) {
-			this.#callbacks.executeIfNoError();
-		}
+    if (!this.#isErrorOccurred()) {
+      this.#callbacks.executeIfNoError();
+    }
 
-		this.#callbacks.finally();
+    this.#callbacks.finally();
 
-		this.#resetCallbacks();
+    return this.#getResult();
+  }
 
-		return this.#result();
-	}
-
-	#resetCallbacks() {
-		this.#callbacks = this.#getDefaultCallbacks();
-	}
-
-	#result() {
-		return this.#hasError ? this.#catchResult : this.#tryResult;
-	}
+  #getResult() {
+    return this.#hasError ? this.#catchResult : this.#tryResult;
+  }
 }
 
 const trier = (callerName, options) => new Trier(callerName, options);
@@ -176,6 +170,6 @@ const trier = (callerName, options) => new Trier(callerName, options);
 module.exports = { trier, Trier };
 
 const printError = (functionName, error) => {
-	console.error(`${functionName} catch, error: `);
-	console.error(error);
+  console.error(`${functionName} catch, error: `);
+  console.error(error);
 };
---
+++ index.test.js
@@ -0,0 +1,214 @@
+const { Trier } = require("./index");
+
+const testResult = "test result";
+const testError = "test error";
+
+describe("Trier", () => {
+  let trier;
+  beforeEach(() => {
+    trier = new Trier("test");
+  });
+
+  describe("try", () => {
+    it("should set the try result", () => {
+      trier.try(() => {
+        return testResult;
+      });
+      expect(trier.run()).toBe(testResult);
+    });
+
+    it("should set the catch result if an error is thrown", () => {
+      trier.try(() => {
+        throw new Error(testError);
+      });
+      expect(trier.run()).toBeInstanceOf(Error);
+    });
+  });
+
+  describe("tryAsync", () => {
+    it("should set the try result", async () => {
+      trier.tryAsync(async () => {
+        return testResult;
+      });
+      expect(await trier.runAsync()).toBe(testResult);
+    });
+
+    it("should set the catch result if an error is thrown", async () => {
+      trier.tryAsync(async () => {
+        throw new Error(testError);
+      });
+      expect(await trier.runAsync()).toBeInstanceOf(Error);
+    });
+  });
+
+  describe("catch", () => {
+    it("should set the catch result", () => {
+      trier
+        .try(() => {
+          throw new Error(testError);
+        })
+        .catch(() => {
+          return "catch result";
+        });
+      expect(trier.run()).toBe("catch result");
+    });
+  });
+
+  describe("executeIfNoError", () => {
+    it("should execute if there is no error", () => {
+      const mockCallback = jest.fn();
+      trier
+        .try(() => {
+          return testResult;
+        })
+        .executeIfNoError(mockCallback);
+      trier.run();
+      expect(mockCallback).toHaveBeenCalledWith(testResult);
+    });
+
+    it("should not execute if there is an error", () => {
+      const mockCallback = jest.fn();
+      trier
+        .try(() => {
+          throw new Error(testError);
+        })
+        .executeIfNoError(mockCallback);
+      trier.run();
+      expect(mockCallback).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("finally", () => {
+    it("should execute finally block", () => {
+      const mockCallback = jest.fn();
+      trier
+        .try(() => {
+          return testResult;
+        })
+        .finally(mockCallback);
+      trier.run();
+      expect(mockCallback).toHaveBeenCalled();
+    });
+  });
+
+  describe("throw", () => {
+    it("should throw an error if an error occurred", () => {
+      trier
+        .try(() => {
+          throw new Error(testError);
+        })
+        .throw();
+      expect(() => trier.run()).toThrow(Error);
+    });
+  });
+
+  describe("run multiple sync", () => {
+    it("should run multiple sync callbacks in try block and execute finally block", () => {
+      const mockFn1 = jest.fn();
+      const mockFn2 = jest.fn();
+      const mockFinally = jest.fn();
+
+      const result = trier.try(mockFn1).try(mockFn2).finally(mockFinally).run();
+
+      expect(mockFn1).toHaveBeenCalled();
+      expect(mockFn2).toHaveBeenCalled();
+      expect(mockFinally).toHaveBeenCalled();
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe("run multiple async", () => {
+    it("should run multiple async callbacks in try block and execute finally block", async () => {
+      const mockFn1 = jest.fn();
+      const mockFn2 = jest.fn();
+      const mockFinally = jest.fn();
+
+      const result = await trier
+        .tryAsync(mockFn1)
+        .tryAsync(mockFn2)
+        .finally(mockFinally)
+        .runAsync();
+
+      expect(mockFn1).toHaveBeenCalled();
+      expect(mockFn2).toHaveBeenCalled();
+      expect(mockFinally).toHaveBeenCalled();
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe("catch error in catch block", () => {
+    it("should catch error in catch block", () => {
+      const mockTryFn = jest.fn(() => {
+        throw new Error(testError);
+      });
+      const mockCatchFn = jest.fn();
+
+      const result = trier
+        .try(mockTryFn)
+        .catch((error) => {
+          mockCatchFn();
+          return error;
+        })
+        .run();
+
+      expect(mockTryFn).toHaveBeenCalled();
+      expect(mockCatchFn).toHaveBeenCalled();
+      expect(result).toBeInstanceOf(Error);
+    });
+  });
+
+  it("should catch error in catch block and throw error", () => {
+    const mockTryFn = jest.fn(() => {
+      throw new Error(testError);
+    });
+    const mockCatchFn = jest.fn();
+    let error;
+
+    try {
+      trier
+        .try(mockTryFn)
+        .catch((error) => {
+          mockCatchFn();
+          return error;
+        })
+        .throw()
+        .run();
+    } catch (err) {
+      error = err;
+    }
+
+    expect(mockTryFn).toHaveBeenCalled();
+    expect(mockCatchFn).toHaveBeenCalled();
+    expect(error).toBeInstanceOf(Error);
+  });
+
+  it("should execute executeIfNoError block if there is no error", () => {
+    const mockTryFn = jest.fn(() => 5);
+    const mockExecuteIfNoErrorFn = jest.fn();
+
+    const result = trier
+      .try(mockTryFn)
+      .executeIfNoError(mockExecuteIfNoErrorFn)
+      .run();
+
+    expect(mockTryFn).toHaveBeenCalled();
+    expect(mockExecuteIfNoErrorFn).toHaveBeenCalledWith(5);
+    expect(result).toBe(5);
+  });
+
+  it("should not execute executeIfNoError block if there is an error", () => {
+    const mockTryFn = jest.fn(() => {
+      throw new Error(testError);
+    });
+    const mockExecuteIfNoErrorFn = jest.fn();
+
+    const result = trier
+      .try(mockTryFn)
+      .executeIfNoError(mockExecuteIfNoErrorFn)
+      .run();
+
+    expect(mockTryFn).toHaveBeenCalled();
+    expect(mockExecuteIfNoErrorFn).not.toHaveBeenCalled();
+    expect(result).toBeInstanceOf(Error);
+  });
+});
